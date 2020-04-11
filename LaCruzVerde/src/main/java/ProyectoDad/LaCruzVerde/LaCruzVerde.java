7ea056c3ad13082e3f3f8267388e545d765b3869
Jorlu <Jorlu@JorluMSIModern14.home>
2020-04-11 16:06:39 +0200

    Cambio LaCruzVerde

--- LaCruzVerde/src/main/java/ProyectoDad/LaCruzVerde/LaCruzVerde.java
+++ LaCruzVerde/src/main/java/ProyectoDad/LaCruzVerde/LaCruzVerde.java
@@ -8,39 +8,8 @@ public class LaCruzVerde extends AbstractVerticle {
 	@SuppressWarnings("deprecation")
 	@Override
 	public void start(Future<Void> startFuture) {
-		//		vertx.createHttpServer().requestHandler(
-		//				request ->{
-		//					request.response().end("<h1>Bienvenido a mi pagina</h1> Hola mundo");
-		//		}).listen(8081, result->{
-		//			if(result.succeeded()) {
-		//				System.out.println("Todo correcto");
-		//			}else {
-		//				System.out.println(result.cause());
-		//			}
-		//		});
-
-		//		vertx.deployVerticle(MySecondVerticle.class.getName());
-		//		vertx.deployVerticle(MyThirdVerticle.class.getName());
-
-		//		vertx.deployVerticle(BlockingThread.class.getName());
 
 		vertx.deployVerticle(DatabaseVerticle.class.getName());
 
-		//		EventBus eventBus=vertx.eventBus();
-		//		vertx.setPeriodic(4000, action->{
-		//			eventBus.send("mensaje_p2p", "Hola, esto es un mensaje �te llega?",reply->{
-		//				if(reply.succeeded()) {
-		//					String replyMessage = 
-		//							(String) reply.result().body();
-		//					System.out.println("Respuesta: "+replyMessage);
-		//				}else {
-		//					System.out.println("No ha habido respuesta");
-		//				}
-		//			});
-		//		});
-		//		
-		//		vertx.setPeriodic(4000, action->{
-		//			eventBus.publish("mensaje_broadcast","Esto es un mensaje broadcast");
-		//		});
 	}
 }
